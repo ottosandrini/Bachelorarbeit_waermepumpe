e7b9c9a0902b1d72a25723f79effcc6d527e2fa5
ottosandrini <so0599@mci4me.at>
2025-05-27 14:33:17 +0200

    gui improvements

--- webpage/illustration.js
+++ webpage/illustration.js
@@ -6,13 +6,14 @@ class HeatpumpVis {
     constructor(type, canvas) {
         this.canvas = canvas;
         // semi-random starting values
-        this.Temperatures = {conn0:10, conn1:60, conn2:55, conn03:3, conn4:3, conn5:10, wq_in:11, wq_out:9, ww_in:11, ww_out:14};
-        this.Pressures = {conn0:8, conn1:22, conn2:22, conn03:8, conn4:8, conn5:8};
-        this.Enthalpies = {conn0:430, conn1:450, conn2:200, conn03:200, conn4:400, conn5:430};
+        this.Temperatures = {conn0:10, conn1:60, conn2:55, conn3:3, conn4:3, conn5:10, wq_in:11, wq_out:9, ww_in:11, ww_out:14};
+        this.Pressures = {conn0:8, conn1:22, conn2:22, conn3:8, conn4:8, conn5:8};
+        this.Enthalpies = {conn0:430, conn1:450, conn2:200, conn3:200, conn4:400, conn5:430};
         this.Powers = {evaporator:4000, condenser:5000, superheater:200, compressor:800};
-        this.MassFlows = {conn0:4.02, conn1:4.02, conn2:4.02, conn03:4.02, conn4:4.02, conn5:4.02};
+        this.MassFlows = {conn0:4.02, conn1:4.02, conn2:4.02, conn03:4.02, conn4:4.02, conn5:4.02, wq:0.475, ww:0.265};
         // Select Simulation or Real Thing with 0 or 1
         this.type = type;
+        this.overheat = 7;
     }
 
     update_data() {
@@ -75,6 +76,7 @@ class HeatpumpVis {
     var evx = 400*xscale+xoffset;
     var evy = 600*yscale+yoffset;
     drawValve(c, evx, evy);
+    drawNames(c);
     line1(c, evax+55,evay,compx-40,compy,0);
     line2(c, compx+40, compy, cdx+55, cdy, 0);
     line3(c, cdx+55, cdy+180, evx+30 ,evy,0);
@@ -85,9 +87,20 @@ class HeatpumpVis {
     document.getElementById("T_wqout").textContent=this.Temperatures.wq_out+"°C";
     document.getElementById("T_wwin").textContent=this.Temperatures.ww_in+"°C";
     document.getElementById("T_wwout").textContent=this.Temperatures.ww_out+"°C";
+    document.getElementById("T_conn1").textContent=this.Temperatures.conn1+"°C";
+    document.getElementById("T_conn2").textContent=this.Temperatures.conn2+"°C";
+    document.getElementById("T_conn3").textContent=this.Temperatures.conn3+"°C";
+    document.getElementById("T_conn4").textContent=this.Temperatures.conn5+"°C";
+    document.getElementById("P_conn1").textContent=this.Pressures.conn1+"bar";
+    document.getElementById("P_conn2").textContent=this.Pressures.conn2+"bar";
+    document.getElementById("P_conn3").textContent=this.Pressures.conn3+"bar";
+    document.getElementById("P_conn4").textContent=this.Pressures.conn5+"bar";
     document.getElementById("P_comp").textContent=this.Powers.compressor+" W";
     document.getElementById("Q_cond").textContent=this.Powers.condenser+" W";
     document.getElementById("Q_evap").textContent=this.Powers.evaporator+" W";
+    document.getElementById("M_wq").textContent=this.MassFlows.wq+"kg/s";
+    document.getElementById("M_ww").textContent=this.MassFlows.ww+"kg/s";
+    document.getElementById("overheat").textContent=this.overheat+"°K";
     }
 }
 
@@ -170,9 +183,9 @@ document.addEventListener("DOMContentLoaded", function () {
         valueBox: false,
         maxValue: 12,
         majorTicks: ["0","2","4","6","8","10", "12"],
-        minorTicks: 5,
+        minorTicks: 6,
         strokeTicks: true,
-        highlights: [{"from": 2,"to": 12,"color": "rgba(10, 180, 50, .55)"}],
+        highlights: [{"from": 3,"to": 12,"color": "rgba(10, 180, 50, .55)"}],
         colorPlate: "#fff",
         borderShadowWidth: 0,
         borders: false,
@@ -195,9 +208,9 @@ document.addEventListener("DOMContentLoaded", function () {
         valueBox: false,
         maxValue: 12,
         majorTicks: ["0","2","4","6","8","10", "12"],
-        minorTicks: 5,
+        minorTicks: 6,
         strokeTicks: true,
-        highlights: [{"from": 2,"to": 12,"color": "rgba(10, 180, 50, .55)"}],
+        highlights: [{"from": 3,"to": 12,"color": "rgba(10, 180, 50, .55)"}],
         colorPlate: "#fff",
         borderShadowWidth: 0,
         borders: false,
@@ -218,9 +231,73 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 
+
+
+// --------------------------------------------------------------------------
+// ------------------ BUTTON FUNCTIONS -------------------------------------
+// --------------------------------------------------------------------------
+
+function buttonclick(element) {
+    element.classList.add("button-clicked");
+    setTimeout(() => {
+        element.classList.remove("button-clicked");
+    }, 50);
+    // if (element.id === "calculate_button") {
+        // calculate something here
+    // }
+    // else {
+        // pass
+    // }
+    // if (element.id === "something_else") {
+        // do something else
+    // }
+}
+
+let tooltip;
+function showtool(element, text) {
+    tooltip = document.createElement("div");
+    tooltip.className = "tooltip";
+    tooltip.innerText = text;
+    document.body.appendChild(tooltip);
+
+    const rect = element.getBoundingClientRect();
+    tooltip.style.top = (window.scrollY + rect.top - tooltip.offsetHeight - 8) + "px";
+    tooltip.style.left = (window.scrollX + rect.right + 8) + "px";
+}
+
+function hidetool() {
+    if (tooltip) {
+    tooltip.remove();
+    tooltip = null;
+    }
+}
+
+function brighten(element) {
+    element.style.filter = "brightness(1.2)";
+}
+
+function unbrighten(element) {
+    element.style.filter = "brightness(1)";
+}
+
+
 // --------------------------------------------------------------------------
 // ------------------ DRAWING FUNCTIONS -------------------------------------
 // --------------------------------------------------------------------------
+
+var basecolor = "rgb(32, 32, 32)";
+var textcolor = "rgb(75, 75, 75)";
+
+function drawNames(c) {
+    c.fillStyle = textcolor;
+    c.strokeStyle = textcolor;
+    c.font = "25px ubuntu";
+    c.fillText("Verdichter", 400, 40);
+    c.fillText("Expansionsventil", 360, 580);
+    c.fillText("Verflüssiger", 760, 230);
+    c.fillText("Verdampfer", 40, 230);
+    c.fillText("Überhitzung", 400, 260);
+}
 
 function drawPoint(c, d, cX, cY) {
     if (d=="rr"){
@@ -270,7 +347,7 @@ function drawPoint(c, d, cX, cY) {
 }
 
 function drawCompressor(c, x, y) {
-    c.strokeStyle = "black";
+    c.strokeStyle = basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.arc(x, y, 40, 0, 2 * Math.PI);
@@ -286,7 +363,7 @@ function drawCompressor(c, x, y) {
 }
 
 function drawHeatExchanger1(c, x, y){
-    c.strokeStyle = "black";
+    c.strokeStyle = basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.rect(x,y, 110, 180);
@@ -297,18 +374,18 @@ function drawHeatExchanger1(c, x, y){
     grad.addColorStop(0,"red");
     c.strokeStyle = grad;
     c.lineWidth = 4;
-    c.moveTo(x+150, y+50);
-    drawPoint(c, "lr",x+150, y+50);
-    c.lineTo(x+30, y+50);
+    c.moveTo(x+150, y+30);
+    drawPoint(c, "lr",x+150, y+30);
+    c.lineTo(x+30, y+30);
     c.lineTo(x+90, y+90);
-    c.lineTo(x+30, y+130);
-    c.lineTo(x+150, y+130);
-    drawPoint(c, "ll", x+150, y+130);
+    c.lineTo(x+30, y+150);
+    c.lineTo(x+150, y+150);
+    drawPoint(c, "ll", x+150, y+150);
     c.stroke();
 }
 
 function drawHeatExchanger2(c, x, y){
-    c.strokeStyle = "black";
+    c.fillStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.rect(x,y, 110, 180);
@@ -319,19 +396,19 @@ function drawHeatExchanger2(c, x, y){
     grad.addColorStop(1,"red");
     c.strokeStyle = grad;
     c.lineWidth = 4;
-    c.moveTo(x-40, y+50);
-    drawPoint(c, "rl", x-40, y+50);
-    c.lineTo(x+80, y+50);
+    c.moveTo(x-40, y+30);
+    drawPoint(c, "rl", x-40, y+30);
+    c.lineTo(x+80, y+30);
     c.lineTo(x+20, y+90);
-    c.lineTo(x+80, y+130);
-    c.lineTo(x-40, y+130);
-    drawPoint(c, "rr", x-40, y+130);
+    c.lineTo(x+80, y+150);
+    c.lineTo(x-40, y+150);
+    drawPoint(c, "rr", x-40, y+150);
     c.stroke();
 }
 
 
 function drawValve(c, x, y) {
-    c.strokeStyle = "black";
+    c.strokeStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.moveTo(x-30, y-15);
@@ -344,7 +421,7 @@ function drawValve(c, x, y) {
 }
 
 function line1(c, x1, y1, x2, y2, r) {
-    c.strokeStyle = "black";
+    c.strokeStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.moveTo(x1,y1);
@@ -353,7 +430,7 @@ function line1(c, x1, y1, x2, y2, r) {
 }
 
 function line2(c, x1, y1, x2, y2, r) {
-    c.strokeStyle = "black";
+    c.strokeStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.moveTo(x1,y1);
@@ -362,7 +439,7 @@ function line2(c, x1, y1, x2, y2, r) {
 }
 
 function line3(c, x1, y1, x2, y2, r) {
-    c.strokeStyle = "black";
+    c.strokeStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.moveTo(x1,y1);
@@ -371,29 +448,10 @@ function line3(c, x1, y1, x2, y2, r) {
 }
 
 function line4(c, x1, y1, x2, y2, r) {
-    c.strokeStyle = "black";
+    c.strokeStyle= basecolor;
     c.lineWidth = 4;
     c.beginPath();
     c.moveTo(x1,y1);
     c.bezierCurveTo(x2+r, y1, x2, y1-r, x2, y2);
     c.stroke();
-}
-let tooltip;
-
-function showtool(element, text) {
-    tooltip = document.createElement("div");
-    tooltip.className = "tooltip";
-    tooltip.innerText = text;
-    document.body.appendChild(tooltip);
-
-    const rect = element.getBoundingClientRect();
-    tooltip.style.top = (window.scrollY + rect.top - tooltip.offsetHeight - 8) + "px";
-    tooltip.style.left = (window.scrollX + rect.right + 8) + "px";
-}
-
-function hidetool() {
-    if (tooltip) {
-    tooltip.remove();
-    tooltip = null;
-    }
 }
